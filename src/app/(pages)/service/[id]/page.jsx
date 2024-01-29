388c26d50eac6d40d7e026efbf45feb283a94598
SaqlineShabbir <saqline5869@gmail.com>
2024-01-29 21:04:49 +0600

    all fetch commented

--- src/app/(pages)/service/[id]/page.jsx
+++ src/app/(pages)/service/[id]/page.jsx
@@ -7,75 +7,76 @@ import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../../../context/AuthProvider';
 
 const page = ({ params }) => {
+
     const [data, setData] = useState('')
     const { user } = useContext(AuthContext);
     //fetch data
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`https://luxury-center.vercel.app/api/service/${params?.id}`, {
-                    method: 'GET'
-                });
-                // Handle the response data here
-                const data = await response.json();
-                setData(data)
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const response = await fetch(`https://luxury-center.vercel.app/api/service/${params?.id}`, {
+    //                 method: 'GET'
+    //             });
+    //             // Handle the response data here
+    //             const data = await response.json();
+    //             setData(data)
 
-            } catch (error) {
-                // Handle errors here
-                console.error('Error fetching data:', error);
-            }
-        };
+    //         } catch (error) {
+    //             // Handle errors here
+    //             console.error('Error fetching data:', error);
+    //         }
+    //     };
 
-        fetchData();
-    }, [params?.id]); // Include id in the dependency array if it's used inside the useEffect callback
+    //     fetchData();
+    // }, [params?.id]);
 
 
 
     //book this service
     const handleBookService = async (e) => {
         e.preventDefault();
-        try {
-            const response = await fetch('https://luxury-center.vercel.app/api/book-service', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    userId: user?._id,
-                    serviceId: data?.service?._id
-                }),
-            });
+        // try {
+        //     const response = await fetch('https://luxury-center.vercel.app/api/book-service', {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json',
+        //         },
+        //         body: JSON.stringify({
+        //             userId: user?._id,
+        //             serviceId: data?.service?._id
+        //         }),
+        //     });
 
-            // Log the complete response for debugging
+        //     // Log the complete response for debugging
 
 
-            // Parse JSON response
-            const jsonResponse = await response.json();
+        //     // Parse JSON response
+        //     const jsonResponse = await response.json();
 
-            if (response.ok) {
-                // Successful booking
-                toast.success('Successfully booked');
-            } else {
-                // Handle error response
-                toast.error(jsonResponse.message || 'You have already booked this service');
-            }
-        } catch (error) {
-            // Handle network or other errors
-            console.error('Error booking service:', error);
-            toast.error('Failed to book service. Please try again.');
-        }
+        //     if (response.ok) {
+        //         // Successful booking
+        //         toast.success('Successfully booked');
+        //     } else {
+        //         // Handle error response
+        //         toast.error(jsonResponse.message || 'You have already booked this service');
+        //     }
+        // } catch (error) {
+        //     // Handle network or other errors
+        //     console.error('Error booking service:', error);
+        //     toast.error('Failed to book service. Please try again.');
+        // }
     };
 
     return (
         <main>
             <Toaster />
             <NavBar></NavBar>
-            <div className='flex flex-col items-center min-h-[90vh]'>
+            {/* <div className='flex flex-col items-center min-h-[90vh]'>
                 <p className='text-2xl text-center'>{data?.service?.title}</p>
                 <p className='text-2xl text-center'>{data?.service?.description}</p>
                 <button onClick={handleBookService}>Book Service</button>
-                {/* <BookServicebtn serviceId={data?.service._id} userId={user?._id} label='Book services'></BookServicebtn> */}
-            </div>
+               
+            </div> */}
             <Footer />
         </main>
     );
--- src/helpers/getServices.js
+++ src/helpers/getServices.js
@@ -1,14 +1,13 @@
-export default async function fetchData() {
-    try {
-        const response = await fetch('https://luxury-center.vercel.app/api/service', {
-            method: 'GET',
-        });
+// export default async function fetchData() {
+//     try {
+//         const response = await fetch('https://luxury-center.vercel.app/api/service', {
+//             method: 'GET',
+//         });
+//         const data = await response.json();
 
-        const data = await response.json();
-
-        return data?.services;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
-    }
-}
+//         return data?.services;
+//     } catch (error) {
+//         console.error('Error fetching data:', error);
+//         throw error;
+//     }
+// }
