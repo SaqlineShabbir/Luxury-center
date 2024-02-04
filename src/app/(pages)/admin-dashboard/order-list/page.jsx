342daaac07d97580805b877c2d609a2e653badac
SaqlineShabbir <saqline5869@gmail.com>
2024-02-04 13:17:21 +0600

    fixing deployment problem

--- src/app/(pages)/admin-dashboard/order-list/page.jsx
+++ src/app/(pages)/admin-dashboard/order-list/page.jsx
@@ -1,115 +1,115 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
-function getStatusColor(status) {
-    switch (status) {
-        case 'Ongoing':
-            return 'text-blue-500 rounded-full';
-        case 'Pending':
-            return 'text-pink-500 rounded-full';
-        case 'Completed':
-            return 'text-green-500 rounded-full';
-        default:
-            return 'text-gray-500';
-    }
-}
+// "use client"
+// import React, { useEffect, useState } from 'react';
+// import toast, { Toaster } from 'react-hot-toast';
+// function getStatusColor(status) {
+//     switch (status) {
+//         case 'Ongoing':
+//             return 'text-blue-500 rounded-full';
+//         case 'Pending':
+//             return 'text-pink-500 rounded-full';
+//         case 'Completed':
+//             return 'text-green-500 rounded-full';
+//         default:
+//             return 'text-gray-500';
+//     }
+// }
 
-const OrderList = () => {
+// const OrderList = () => {
 
 
-    //Fetch all data
-    const [data, setData] = useState([]);
-    const [statusState, setStatusState] = useState('Pending')
-    console.log(statusState)
+//     //Fetch all data
+//     const [data, setData] = useState([]);
+//     const [statusState, setStatusState] = useState('Pending')
+//     console.log(statusState)
 
-    const fetchData = async () => {
-        try {
-            const response = await fetch('https://luxury-center.vercel.app/api/book-service', {
-                method: 'GET'
-            });
+//     const fetchData = async () => {
+//         try {
+//             const response = await fetch('https://luxury-center.vercel.app/api/book-service', {
+//                 method: 'GET'
+//             });
 
-            // Assuming the response is in JSON format, you can extract data like this:
-            const data = await response.json();
-            setData(data?.booking)
-            // Log the data to the console
-            console.log(data?.booking);
+//             // Assuming the response is in JSON format, you can extract data like this:
+//             const data = await response.json();
+//             setData(data?.booking)
+//             // Log the data to the console
+//             console.log(data?.booking);
 
-        } catch (error) {
-            // Handle any errors that occur during the fetch operation
-            console.error('Error fetching data:', error.message);
-        }
-    };
+//         } catch (error) {
+//             // Handle any errors that occur during the fetch operation
+//             console.error('Error fetching data:', error.message);
+//         }
+//     };
 
-    // Call the fetchData function when the component mounts (empty dependency array)
-    useEffect(() => {
-        fetchData();
-    }, []);
+//     // Call the fetchData function when the component mounts (empty dependency array)
+//     useEffect(() => {
+//         fetchData();
+//     }, []);
 
-    //update existing data
-    const handleChangeStatus = async (e, id) => {
-        setStatusState(e.target.value)
-        try {
-            const response = await fetch(`https://luxury-center.vercel.app/api/book-service/${id}`, {
-                method: 'PATCH',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ status: statusState }),
-            });
+//     //update existing data
+//     const handleChangeStatus = async (e, id) => {
+//         setStatusState(e.target.value)
+//         try {
+//             const response = await fetch(`https://luxury-center.vercel.app/api/book-service/${id}`, {
+//                 method: 'PATCH',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                 },
+//                 body: JSON.stringify({ status: statusState }),
+//             });
 
-            if (response.ok) {
-                fetchData()
-                toast.success('Updated Successfully')
-                console.log(response)
-            } else {
-                console.error(`Failed to update status for ID: ${id}`);
-            }
+//             if (response.ok) {
+//                 fetchData()
+//                 toast.success('Updated Successfully')
+//                 console.log(response)
+//             } else {
+//                 console.error(`Failed to update status for ID: ${id}`);
+//             }
 
-        } catch (error) {
-            console.error('Error updating status:', error.message);
-        }
+//         } catch (error) {
+//             console.error('Error updating status:', error.message);
+//         }
 
-    }
+//     }
 
-    return (
-        <div className="container mx-auto mt-8 overflow-x-auto">
-            <Toaster />
-            <table className="min-w-full bg-green-50 rounded">
-                <thead>
-                    <tr>
-                        <th className="py-2 border-b text-lg text-slate-500">Name</th>
-                        <th className="py-2 border-b text-lg text-slate-500 hidden md:block">Email</th>
-                        <th className="py-2 border-b text-lg text-slate-500">Service Title</th>
-                        {/* <th className="py-2 border-b text-lg text-slate-500 hidden md:block">Pay With</th> */}
-                        <th className="py-2 border-b text-lg text-slate-500">Status</th>
-                    </tr>
-                </thead>
+//     return (
+//         <div className="container mx-auto mt-8 overflow-x-auto">
+//             <Toaster />
+//             <table className="min-w-full bg-green-50 rounded">
+//                 <thead>
+//                     <tr>
+//                         <th className="py-2 border-b text-lg text-slate-500">Name</th>
+//                         <th className="py-2 border-b text-lg text-slate-500 hidden md:block">Email</th>
+//                         <th className="py-2 border-b text-lg text-slate-500">Service Title</th>
+//                         {/* <th className="py-2 border-b text-lg text-slate-500 hidden md:block">Pay With</th> */}
+//                         <th className="py-2 border-b text-lg text-slate-500">Status</th>
+//                     </tr>
+//                 </thead>
 
-                <tbody>
-                    {data?.map((item) => (
-                        <tr key={item?._id}>
-                            <td className="py-2 px-4 border-b text-slate-500">{item?.user.firstname}{item?.user.lastname}</td>
-                            <td className="py-2 px-4 border-b text-slate-500 hidden md:block">{item?.user.email}</td>
-                            <td className="py-2 px-4 border-b text-slate-500">{item?.service?.title}</td>
-                            {/* <td className="py-2 px-4 border-b text-slate-500 hidden md:block">{item?.payWith}</td> */}
-                            <td className={`py-2 px-4 border-b ${getStatusColor(item?.status)}`}>
+//                 <tbody>
+//                     {data?.map((item) => (
+//                         <tr key={item?._id}>
+//                             <td className="py-2 px-4 border-b text-slate-500">{item?.user.firstname}{item?.user.lastname}</td>
+//                             <td className="py-2 px-4 border-b text-slate-500 hidden md:block">{item?.user.email}</td>
+//                             <td className="py-2 px-4 border-b text-slate-500">{item?.service?.title}</td>
+//                             {/* <td className="py-2 px-4 border-b text-slate-500 hidden md:block">{item?.payWith}</td> */}
+//                             <td className={`py-2 px-4 border-b ${getStatusColor(item?.status)}`}>
 
-                                <select
-                                    className={`px-2 py-1 ${getStatusColor(item?.status)}`}
-                                    value={item?.status}
-                                    onChange={(e) => handleChangeStatus(e, item?._id)}
-                                >
-                                    <option value="Pending">Pending</option>
-                                    <option value="Ongoing">Ongoing</option>
-                                    <option value="Completed">Completed</option>
-                                </select>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
-};
+//                                 <select
+//                                     className={`px-2 py-1 ${getStatusColor(item?.status)}`}
+//                                     value={item?.status}
+//                                     onChange={(e) => handleChangeStatus(e, item?._id)}
+//                                 >
+//                                     <option value="Pending">Pending</option>
+//                                     <option value="Ongoing">Ongoing</option>
+//                                     <option value="Completed">Completed</option>
+//                                 </select>
+//                             </td>
+//                         </tr>
+//                     ))}
+//                 </tbody>
+//             </table>
+//         </div>
+//     );
+// };
 
-export default OrderList;
+// export default OrderList;
--- src/app/(pages)/dashboard/booking-list/page.jsx
+++ src/app/(pages)/dashboard/booking-list/page.jsx
@@ -1,100 +1,100 @@
-'use client'
-import { MdDeleteForever } from "react-icons/md";
-import { AuthContext } from "../../../../context/AuthProvider";
-import { useContext, useEffect, useState } from "react";
-import toast, { Toaster } from "react-hot-toast";
+// 'use client'
+// import { MdDeleteForever } from "react-icons/md";
+// import { AuthContext } from "../../../../context/AuthProvider";
+// import { useContext, useEffect, useState } from "react";
+// import toast, { Toaster } from "react-hot-toast";
 
 
-const page = async () => {
-    const { user } = useContext(AuthContext);
-    const [data, setData] = useState()
+// const page = async () => {
+//     const { user } = useContext(AuthContext);
+//     const [data, setData] = useState()
 
-    const fetchData = async () => {
-        try {
-            const response = await fetch(`https://luxury-center.vercel.app/api/book-service?userId=${user?._id}`, {
-                method: 'GET'
-            });
+//     const fetchData = async () => {
+//         try {
+//             const response = await fetch(`https://luxury-center.vercel.app/api/book-service?userId=${user?._id}`, {
+//                 method: 'GET'
+//             });
 
-            // Handle the response data here
-            const data = await response.json();
-            console.log('user', data)
-            setData(data?.booking)
-
-
-            // You can process 'data' as needed (e.g., update state)
-        } catch (error) {
-            // Handle errors here
-            console.error('Error fetching data:', error);
-        }
-    };
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    //cancel booking
-    const handleCancelBooking = async (id) => {
-
-        try {
-            const response = await fetch(`https://luxury-center.vercel.app/api/book-service/${id}`, {
-                method: 'DELETE',
-            });
-            if (response.status === 200) {
-                toast.success('Booking cancled Successfull')
-                fetchData()
-
-            }
-            console.log('Booking canceled successfully', response);
-            // Perform any other necessary actions (e.g., update state, show a notification)
+//             // Handle the response data here
+//             const data = await response.json();
+//             console.log('user', data)
+//             setData(data?.booking)
 
 
-        } catch (error) {
-            console.error('Error cancelling booking:', error.message);
-            toast.error(error.message)
-            setError(`Error cancelling booking: ${error.message}`);
-            // Handle errors (e.g., show an error message to the user)
-        }
-    };
+//             // You can process 'data' as needed (e.g., update state)
+//         } catch (error) {
+//             // Handle errors here
+//             console.error('Error fetching data:', error);
+//         }
+//     };
+//     useEffect(() => {
+//         fetchData();
+//     }, []);
 
-    let content = null
-    if (data?.length <= 0) {
-        content = <div><p className="text-center">No booking exist </p></div>
-    }
+//     //cancel booking
+//     const handleCancelBooking = async (id) => {
 
-    if (data?.length > 0) {
-        content = <div className='bg-green-50 w-full p-4 grid grid-cols-1 md:grid-cols-3 gap-4 rounded shadow'>
-            {
-                data?.map((selectService) => (
-                    <div key={selectService?._id} className="bg-white p-6 rounded-lg space-y-4 shadow">
-                        <div onClick={() => handleCancelBooking(selectService?._id)} className="flex justify-end hover:text-red-400"><MdDeleteForever size={20} /></div>
-                        <div className="flex justify-between items-center">
-                            {/* <Image
-                            src={selectService?.image}
-                            alt="service icon"
-                            className="w-16 h-16"
-                        /> */}
+//         try {
+//             const response = await fetch(`https://luxury-center.vercel.app/api/book-service/${id}`, {
+//                 method: 'DELETE',
+//             });
+//             if (response.status === 200) {
+//                 toast.success('Booking cancled Successfull')
+//                 fetchData()
 
-                            <button className="px-4 py-2 rounded bg-green-100 text-pink-500">{selectService?.status}</button>
-                        </div>
+//             }
+//             console.log('Booking canceled successfully', response);
+//             // Perform any other necessary actions (e.g., update state, show a notification)
 
-                        <h2 className="text-xl text-slate-700 font-bold">{selectService?.service?.title}</h2>
-                        <p className="text-slate-500 text-sm">{selectService?.service?.description}</p>
 
-                    </div>
-                ))
-            }
+//         } catch (error) {
+//             console.error('Error cancelling booking:', error.message);
+//             toast.error(error.message)
+//             setError(`Error cancelling booking: ${error.message}`);
+//             // Handle errors (e.g., show an error message to the user)
+//         }
+//     };
 
-        </div>
-    }
-    return (
-        <main>
-            <Toaster />
-            <section className='py-10'>
-                <div className='flex justify-start items-center'>
-                    {content}
-                </div>
-            </section>
-        </main>
-    )
-}
+//     let content = null
+//     if (data?.length <= 0) {
+//         content = <div><p className="text-center">No booking exist </p></div>
+//     }
 
-export default page
+//     if (data?.length > 0) {
+//         content = <div className='bg-green-50 w-full p-4 grid grid-cols-1 md:grid-cols-3 gap-4 rounded shadow'>
+//             {
+//                 data?.map((selectService) => (
+//                     <div key={selectService?._id} className="bg-white p-6 rounded-lg space-y-4 shadow">
+//                         <div onClick={() => handleCancelBooking(selectService?._id)} className="flex justify-end hover:text-red-400"><MdDeleteForever size={20} /></div>
+//                         <div className="flex justify-between items-center">
+//                             {/* <Image
+//                             src={selectService?.image}
+//                             alt="service icon"
+//                             className="w-16 h-16"
+//                         /> */}
+
+//                             <button className="px-4 py-2 rounded bg-green-100 text-pink-500">{selectService?.status}</button>
+//                         </div>
+
+//                         <h2 className="text-xl text-slate-700 font-bold">{selectService?.service?.title}</h2>
+//                         <p className="text-slate-500 text-sm">{selectService?.service?.description}</p>
+
+//                     </div>
+//                 ))
+//             }
+
+//         </div>
+//     }
+//     return (
+//         <main>
+//             <Toaster />
+//             <section className='py-10'>
+//                 <div className='flex justify-start items-center'>
+//                     {content}
+//                 </div>
+//             </section>
+//         </main>
+//     )
+// }
+
+// export default page
