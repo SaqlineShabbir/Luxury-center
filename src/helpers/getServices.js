f53201cb5b89da54c2236ae14a3d06f8d1755d2b
SaqlineShabbir <saqline5869@gmail.com>
2024-02-05 13:42:42 +0600

    fixing deployment error

--- src/helpers/getServices.js
+++ src/helpers/getServices.js
@@ -1,16 +1,10 @@
 export default async function fetchData() {
     try {
-        const timeoutPromise = new Promise((_, reject) => {
-            setTimeout(() => {
-                reject(new Error('Request timed out'));
-            }, 10000); // Adjust the timeout value (in milliseconds) as needed
-        });
-
-        const fetchPromise = fetch('https://luxury-center.vercel.app/api/service', {
+        const response = await fetch('https://luxury-center.vercel.app/api/service', {
             method: 'GET',
-        }).then(response => response.json());
+        });
+        const data = await response.json();
 
-        const data = await Promise.race([fetchPromise, timeoutPromise]);
 
         return data?.services;
     } catch (error) {
